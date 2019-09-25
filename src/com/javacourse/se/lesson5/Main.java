7531edf2b34a7b5cde26581318192386c3ca0ecc
Danila <Danila00000@Gmail.com>
2019-09-25 08:33:35 +0700

    add lesson 9

--- src/com/javacourse/se/lesson5/Main.java
+++ src/com/javacourse/se/lesson5/Main.java
@@ -1,21 +1,32 @@
 package com.javacourse.se.lesson5;
 
-import com.javacourse.se.lesson7.Bus; // импорт пакета в котором находится нужный нам класс
+//import com.javacourse.se.lesson7.Bus; // импорт пакета в котором находится нужный нам класс
+import com.javacourse.se.lesson9.Operation;
 
 public class Main {
     public static void main(String[] args) {
-        Bus ourBus = new Bus(); // объявление и инициализация объекта
-        Bus firstBus = new Bus(); // объявление и инициализация объекта
-        Bus secondBus = new Bus(); // объявление и инициализация объекта
+    Operation op = new Operation();
+    op.foo();
 
-        // задаем поля значения
-        ourBus.color = "Green";
-        firstBus.color = "Red";
-        secondBus.color = "Grey";
 
-        // выводим метод showColor()
-        ourBus.showColor();
-        firstBus.showColor();
-        secondBus.showColor();
+
+
+
+
+
+        //lesson7
+//        Bus ourBus = new Bus(); // объявление и инициализация объекта
+//        Bus firstBus = new Bus(); // объявление и инициализация объекта
+//        Bus secondBus = new Bus(); // объявление и инициализация объекта
+//
+//        // задаем поля значения
+//        ourBus.color = "Green";
+//        firstBus.color = "Red";
+//        secondBus.color = "Grey";
+//
+//        // выводим метод showColor()
+//        ourBus.showColor();
+//        firstBus.showColor();
+//        secondBus.showColor();
     }
 }
