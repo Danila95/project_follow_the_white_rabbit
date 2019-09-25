c8f7cac3a758e1cda0a4b9f8ab3e7d01bebbca93
Danila <Danila00000@Gmail.com>
2019-09-25 09:07:49 +0700

    add lesson10

--- src/com/javacourse/se/lesson5/Main.java
+++ src/com/javacourse/se/lesson5/Main.java
@@ -1,19 +1,23 @@
 package com.javacourse.se.lesson5;
 
 //import com.javacourse.se.lesson7.Bus; // импорт пакета в котором находится нужный нам класс
-import com.javacourse.se.lesson9.Operation;
+//import com.javacourse.se.lesson9.Operation;
+import com.javacourse.se.lesson10.ControlOperators;
 
 public class Main {
     public static void main(String[] args) {
-    Operation op = new Operation();
-    op.foo();
 
 
 
 
 
 
-
+        // lesson10
+//        ControlOperators co = new ControlOperators();
+//        co.foo();
+        // lesson9
+//    Operation op = new Operation();
+//    op.foo();
         //lesson7
 //        Bus ourBus = new Bus(); // объявление и инициализация объекта
 //        Bus firstBus = new Bus(); // объявление и инициализация объекта
